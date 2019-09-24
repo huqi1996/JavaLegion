261665b98cecd808d953d29acde2ff1f03f889b4
huqi <qi.hu@zuolin.com>
2019-09-24 18:06:49 +0800

    demo

--- src/main/java/com/huqi/qs/main/Main.java
+++ src/main/java/com/huqi/qs/main/Main.java
@@ -5,6 +5,7 @@ import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -16,21 +17,42 @@ import java.util.Map;
  * @author huqi
  */
 public class Main {
-    public static void main(String[] args) {
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost("localhost", 9200, "http")).build());
+
+    public static RestHighLevelClient client = new RestHighLevelClient(
+            RestClient.builder(new HttpHost("localhost", 9200, "http")).build());
+
+    public static void indexByMap() {
         Map<String, Object> jsonMap = new HashMap<>();
         jsonMap.put("user", "huqi");
         jsonMap.put("postDate", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now()));
-        jsonMap.put("message", "trying out Elasticsearch");
+        jsonMap.put("message", "trying out Elasticsearch 001");
         IndexRequest indexRequest = new IndexRequest("index", "type", "1")
                 .source(jsonMap);
+        processIndexRequest(indexRequest);
+    }
+
+    public static void indexByJson() {
+        String jsonString = "{" +
+                "\"user\":\"huqi\"," +
+                "\"postDate\":\"2019-09-24 17:44:01\"," +
+                "\"message\":\"trying out Elasticsearch 002\"" +
+                "}";
+        IndexRequest indexRequest = new IndexRequest("index", "type", "2")
+                .source(jsonString, XContentType.JSON);
+        processIndexRequest(indexRequest);
+    }
+
+    public static void processIndexRequest(IndexRequest indexRequest) {
         try {
             IndexResponse indexResponse = client.index(indexRequest);
             System.out.println(indexResponse.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        indexByMap();
+        indexByJson();
     }
 }
